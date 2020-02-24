1520f43267d98376225c31d5b2983b87f05ef76a
armada0838 <54798658+armada0838@users.noreply.github.com>
2020-02-24 11:53:24 +0100

    Begin

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+// begin hier met jouw code voor deze opdracht
+let myPoint = new Point(new Vector2d(200,100),20,3);
